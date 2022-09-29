b79fe538291a556615520d5927102dadf9b3a77e
Willie Henderson <wm2727@ou.ac.uk>
2022-09-29 16:45:39 +0100

    button does something

---
+++ scripts/script-calculator.js
@@ -0,0 +1,30 @@
+// Gets
+const num1 = document.querySelector(".numberButton1");
+const num2 = document.querySelector(".numberButton2");
+const num3 = document.querySelector(".numberButton3");
+const num4 = document.querySelector(".numberButton4");
+const num5 = document.querySelector(".numberButton5");
+const num6 = document.querySelector(".numberButton6");
+const num7 = document.querySelector(".numberButton7");
+const num8 = document.querySelector(".numberButton8");
+const num9 = document.querySelector(".numberButton9");
+const num0 = document.querySelector(".numberButton0");
+const numdot = document.querySelector(".numberButtonDot");
+
+// Functions
+const trialAlert = (event) => {
+  alert("You clicked a button");
+}
+
+// Event Listeners
+num1.addEventListener("click", trialAlert);
+num2.addEventListener("click", trialAlert);
+num3.addEventListener("click", trialAlert);
+num4.addEventListener("click", trialAlert);
+num5.addEventListener("click", trialAlert);
+num6.addEventListener("click", trialAlert);
+num7.addEventListener("click", trialAlert);
+num8.addEventListener("click", trialAlert);
+num9.addEventListener("click", trialAlert);
+num0.addEventListener("click", trialAlert);
+numdot.addEventListener("click", trialAlert);
